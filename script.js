c45c728786473924c68c7627a64a79869b4989fe
habibakhan44 <152440245+habibakhan44@users.noreply.github.com>
2024-10-05 18:35:06 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,95 @@
+// QUESTION 1
+// var input=prompt('Enter a number or String');
+// var numInput = Number (input);
+// var asciiCode = input.charCodeAt(0);
+// if (Number.isInteger(numInput) == true){
+//     console.log(input  + ' is a number');
+
+// }else if (asciiCode>=65 && asciiCode<=90){
+//     console.log(input  +  ' is an uppercase letter');
+
+// }else if (asciiCode>=97 && asciiCode<=122){
+//     console.log(input + ' is an lowercase letter');
+
+// }else {
+//     console.log('Invalid input');
+
+// }
+
+// QUESTION 2
+// var num1 = Number (prompt("Enter the First integer"));
+// var num2 = Number(prompt("Enter the second integer"));
+// if (num1 > num2) {
+//   console.log("The larger of " + num1 + " and " + num2 + " is " + num1 + ".");
+// } else if (num2 > num1) {
+//   console.log("The larger of " + num1 + " and " + num2 + " is " + num2 + ".");
+// } else {
+//   console.log("The values " + num1 + " and " + num2 + " are equal.");
+// }
+
+// QUESTION 3
+
+// var num = Number(prompt("Enter a number"));
+// if (num > 0) {
+//   console.log(num + " is a positive number.");
+// } else if (num < 0) {
+//   console.log(num + " is a negative number.");
+// } else {
+//   console.log(num + " is equal to zero.");
+// }
+
+// QUESTION 4
+// var input = prompt('Enter a character');
+// if (input.length === 1 && (input === 'a' || input === 'e' || input === 'i' || input === 'o' || input === 'u'
+//     || input === 'A' || input === 'E' || input === 'I' || input === 'O' || input === 'U')) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+
+// QUESTION 5
+// var myPass = "habibak1244"; 
+// var inputPass = prompt("Enter a Password");
+// if (inputPass === "") {
+//   console.log("Please enter your password");
+// } else if (inputPass == myPass) {
+//   console.log("Correct! The password you entered matches the original password.");
+// } else {
+//   console.log("Incorrect password");
+// }
+
+// QUESTION 6
+// var greeting;
+// var hour = 13;
+// if (hour < 18) {
+//   greeting = "Good day";
+//   console.log(greeting);
+// } else {
+//   greeting = "Good evening";
+//   console.log(greeting);
+// }
+
+
+// QUESTION 7
+// var timeInput = prompt("Enter time in 24-hour format ");
+// var hour = timeInput;
+// hour = Number(hour);
+// if (timeInput.length !== 4) {
+//   console.log("Invalid input. Please enter time in 24-hour format (e.g., 1900).");
+// } else if (hour < 1200) {
+//       console.log("Good morning!");
+// } else if (hour < 1700) {
+//       console.log("Good afternoon!");
+// } else if (hour < 2000) {
+//       console.log("Good evening!");
+// } else {
+//       console.log("Good night!");
+// }
+
+
+
+
+
+  
+
